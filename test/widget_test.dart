7dcd7318ce38346b69e6c3f8dda031e3a69601a8
Dev Keshwani <dev.keshwani345@gmail.com>
2023-12-23 21:00:33 +0530

    build: added dev dependencies and initialised app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bluetooth_door_system_app/main.dart';
+import 'package:bluetooth_door_system_app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
